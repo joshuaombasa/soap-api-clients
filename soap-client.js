134fa9f6f77d6d9eca022107e975ea4a8774d5f9
Joshua Ombasa <114947718+joshuaombasa@users.noreply.github.com>
2025-01-28 23:06:57 +0300

    Create soap-client.js

---
+++ soap-client.js
@@ -0,0 +1,22 @@
+const soap = require('soap');
+
+// Define WSDL URL
+const wsdlUrl = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL";
+
+// Create a SOAP client
+soap.createClient(wsdlUrl, (err, client) => {
+    if (err) {
+        console.error("Error creating SOAP client:", err);
+        return;
+    }
+
+    // Call the function to get the capital city of Kenya
+    client.CapitalCity({ sCountryISOCode: 'KE' }, (err, result) => {
+        if (err) {
+            console.error("Error calling SOAP service:", err);
+            return;
+        }
+
+        console.log(`The capital city of Kenya is: ${result?.CapitalCityResult}`);
+    });
+});
